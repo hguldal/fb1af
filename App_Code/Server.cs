fb1d81810eb3667fb8317ee4b2792ee532fb9846
hguldal <hguldal@gmail.com>
2016-05-24 16:22:11 +0300

    12aedcf

---
+++ App_Code/Server.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using WebMatrix.Data;
+
+public class Kisi
+{
+   
+   private string Rumuz;
+   
+   public string Rumuz
+   {
+       get { return Rumuz; }
+       set { Rumuz = value; }
+   }
+
+
+}
+
+public class WebRtcHub3: Hub {
+    public void Send(string message) {
+       
+        Clients.All.onMessageReceived(message);
+    }
+}
+
+public class bulutDersChat : Hub
+{
+   
+    public Task gorusmeyeKatil(string KanalKod,string Rumuz)
+    {
+   
+        Groups.Add(Context.ConnectionId, KanalKod); 
+        return Clients.Group(KanalKod).gorusmeyeKatildiIstemci(Rumuz,Context.ConnectionId);
+       
+    }
+
+    public Task gorusmedenAyril(string KanalKod)
+    {
+        return Groups.Remove(Context.ConnectionId, KanalKod);
+    }
+
+    public void mesajGonder(string KanalKod, string Rumuz, string mesaj)
+    {
+
+        Clients.Group(KanalKod).mesajGonderIstemci(Rumuz, mesaj);
+    }
+
+    public override System.Threading.Tasks.Task OnDisconnected()
+        {
+            
+            var db=Database.Open("fb1ae");
+            var queryOnlineKullanici=db.QuerySingle("SELECT * FROM MesajlasmaKanaliOnlineKullanicilar WHERE ConnID=@0",Context.ConnectionId);
+            Clients.Group(Convert.ToString(queryOnlineKullanici.KanalKod)).kullaniciCikti(Convert.ToString(Context.ConnectionId));
+            db.Execute("DELETE FROM MesajlasmaKanaliOnlineKullanicilar WHERE ConnID=@0",Context.ConnectionId);
+            db.Close();
+           
+            return base.OnDisconnected();
+        }
+
+}
+
---
+++ App_Code/Sifreleme.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.IO;
+using System.Text;
+using System.Configuration;
+using System.Security.Cryptography;
+
+/// <summary>
+/// Summary description for Sifreleme
+/// </summary>
+public class Sifreleme
+{
+        public static string Sifrele(string clearText)
+        {
+            
+            string EncryptionKey = System.Configuration.ConfigurationManager.AppSettings["AESKey"];
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
+
+    public static string Coz(string cipherText)
+    {
+        string EncryptionKey = System.Configuration.ConfigurationManager.AppSettings["AESKey"];
+        byte[] cipherBytes = Convert.FromBase64String(cipherText);
+        using (Aes encryptor = Aes.Create())
+        {
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+            encryptor.Key = pdb.GetBytes(32);
+            encryptor.IV = pdb.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(cipherBytes, 0, cipherBytes.Length);
+                    cs.Close();
+                }
+                cipherText = Encoding.Unicode.GetString(ms.ToArray());
+            }
+        }
+        return cipherText;
+    }
+}
